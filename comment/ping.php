c14428325ae742e4ddb942ac94d23de6d4bbebb7
David Chandra Purnama <david@shellcreeper.com>
2015-07-04 21:20:35 +0700

    Remove title from time tag.

--- comment/ping.php
+++ comment/ping.php
@@ -4,7 +4,7 @@
 
 		<cite class="comment-author" itemprop="creator" itemscope="itemscope" itemtype="http://schema.org/Person"><?php comment_author_link(); ?></cite><br />
 
-		<time class="comment-published" datetime="<?php echo get_comment_time( 'Y-m-d\TH:i:sP' );?>" title="<?php echo get_comment_time( 'l, F j, Y, g:i a' ); ?>" itemprop="commentTime"><?php printf( '%1$s (%2$s)', get_comment_date(), get_comment_time() ) ?></time>
+		<time class="comment-published" datetime="<?php echo get_comment_time( 'Y-m-d\TH:i:sP' );?>" itemprop="commentTime"><?php printf( '%1$s (%2$s)', get_comment_date(), get_comment_time() ) ?></time>
 
 		<a class="comment-permalink" href="<?php echo esc_url( get_comment_link() ); ?>" itemprop="url">#</a>
 
--- content/post.php
+++ content/post.php
@@ -10,7 +10,7 @@
 
 				<span class="entry-author" itemprop="author" itemscope="itemscope" itemtype="http://schema.org/Person"><?php the_author_posts_link(); ?></span>
 
-				<time class="entry-published updated" datetime="<?php echo get_the_time( 'Y-m-d\TH:i:sP' ); ?>" title="<?phpecho get_the_time( 'l, F j, Y, g:i a' );?>"><?php echo get_the_date(); ?></time>
+				<time class="entry-published updated" datetime="<?php echo get_the_time( 'Y-m-d\TH:i:sP' ); ?>"><?php echo get_the_date(); ?></time>
 
 				<?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
 
