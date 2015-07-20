7d3c42696ea3a72237945ef3e700b8b70c86b3a8
David Chandra Purnama <david@shellcreeper.com>
2015-07-20 09:42:49 +0700

    remove link in entry published.

--- content/post-singular.php
+++ content/post-singular.php
@@ -10,7 +10,7 @@
 
 				<span class="entry-author"><?php the_author_posts_link(); ?></span>
 
-				<time class="entry-published updated" datetime="<?php echo get_the_time( 'Y-m-d\TH:i:sP' ); ?>"><a href="<?php the_permalink(); ?>"><?php echo get_the_date(); ?></a></time>
+				<time class="entry-published updated" datetime="<?php echo get_the_time( 'Y-m-d\TH:i:sP' ); ?>"><?php echo get_the_date(); ?></time>
 
 				<?php comments_popup_link( number_format_i18n( 0 ), number_format_i18n( 1 ), '%', 'comments-link', '' ); ?>
 
