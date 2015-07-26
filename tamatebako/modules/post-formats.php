5b185dd1341a4d1f62ff4cbf95ccf64e9dde4fc1
David Chandra Purnama <david@shellcreeper.com>
2015-07-26 11:15:00 +0700

    post format chat markup.

--- tamatebako/modules/post-formats.php
+++ tamatebako/modules/post-formats.php
@@ -94,7 +94,7 @@ function tamatebako_get_the_post_format_chat( $content ) {
 	$separator = ':';
 
 	/* Split the content to get individual chat rows. */
-	$chat_rows = preg_split( "/(\r?\n)+|(<br\s*\/?>\s*)+/", $content );
+	$chat_rows = preg_split( "/(\r?\n)+|(<br\s*\/?>\s*)+/", trim( $content ) );
 
 	/* Loop through each row and format the output. */
 	foreach ( $chat_rows as $chat_row ) {
@@ -126,7 +126,7 @@ function tamatebako_get_the_post_format_chat( $content ) {
 
 			/* Make sure we have text. */
 			if ( !empty( $chat_row ) ) {
-				$stanza[] = array( 'message' => $chat_row );
+				$stanza[] = array( 'message' => trim( $chat_row ) );
 			}
 		}
 
@@ -166,20 +166,29 @@ function tamatebako_post_format_chat_content( $content ) {
 			$chat_text   = $row['message'];
 
 			/* Get the speaker/row ID. */
+			global $_tamatebako_chat_id;
 			$speaker_id = tamatebako_chat_row_id( $chat_author );
 
 			/* Open the chat row. */
 			$chat_output .= "\n\t\t\t\t" . '<div class="chat-row ' . sanitize_html_class( "chat-speaker-{$speaker_id}" ) . '">';
 
 			/* Add the chat row author. */
-			if ( !empty( $chat_author ) )
-				$chat_output .= "\n\t\t\t\t\t" . '<div class="chat-author ' . sanitize_html_class( strtolower( "chat-author-{$chat_author}" ) ) . ' vcard"><cite class="fn">' . $chat_author . '</cite>:</div>';
+			if ( !empty( $chat_author ) ){
+				if( isset( $_tamatebako_chat_id ) && $_tamatebako_chat_id == $speaker_id ){
+					$chat_output .= "\n\t\t\t\t\t" . '<div class="chat-author chat-author-repeat ' . sanitize_html_class( strtolower( "chat-author-{$chat_author}" ) ) . ' vcard"><cite class="fn">' . $chat_author . '</cite><span class="chat-sep">:</span></div>';
+				}
+				else{
+					$chat_output .= "\n\t\t\t\t\t" . '<div class="chat-author ' . sanitize_html_class( strtolower( "chat-author-{$chat_author}" ) ) . ' vcard"><cite class="fn">' . $chat_author . '</cite><span class="chat-sep">:</span></div>';
+				}
+			}
 
 			/* Add the chat row text. */
-			$chat_output .= "\n\t\t\t\t\t" . '<div class="chat-text">' . str_replace( array( "\r", "\n", "\t" ), '', wpautop($chat_text ) ) . '</div>';
+			$chat_output .= "\n\t\t\t\t\t" . '<div class="chat-text">' . str_replace( array( "\r", "\n", "\t" ), '', wpautop( $chat_text )  ) . '</div>';
 
 			/* Close the chat row. */
 			$chat_output .= "\n\t\t\t\t" . '</div><!-- .chat-row -->';
+
+			$_tamatebako_chat_id = $speaker_id;
 		}
 	}
 
@@ -187,8 +196,9 @@ function tamatebako_post_format_chat_content( $content ) {
 	$chat_output .= "\n\t\t\t</div><!-- .chat-transcript -->\n";
 
 	/* Reset Chat ID for new post */
-	global $_tamatebako_chat_ids;
+	global $_tamatebako_chat_ids, $_tamatebako_chat_id;
 	$_tamatebako_chat_ids = array();
+	$_tamatebako_chat_id = '';
 
 	/* Return the chat content. */
 	return $chat_output;
@@ -201,15 +211,22 @@ function tamatebako_post_format_chat_content( $content ) {
 function tamatebako_chat_row_id( $chat_author ) {
 	global $_tamatebako_chat_ids;
 
-	/* Let's sanitize the chat author to avoid craziness and differences like "John" and "john". */
-	$chat_author = strtolower( strip_tags( $chat_author ) );
+	if ( !empty( $chat_author ) ){
 
-	/* Add the chat author to the array. */
-	$_tamatebako_chat_ids[] = $chat_author;
+		/* Let's sanitize the chat author to avoid craziness and differences like "John" and "john". */
+		$chat_author = strtolower( strip_tags( $chat_author ) );
 
-	/* Make sure the array only holds unique values. */
-	$_tamatebako_chat_ids = array_unique( $_tamatebako_chat_ids );
+		/* Add the chat author to the array. */
+		$_tamatebako_chat_ids[] = $chat_author;
 
-	/* Return the array key for the chat author and add "1" to avoid an ID of "0". */
-	return absint( array_search( $chat_author, $_tamatebako_chat_ids ) ) + 1;
+		/* Make sure the array only holds unique values. */
+		$_tamatebako_chat_ids = array_unique( $_tamatebako_chat_ids );
+
+		/* Return the array key for the chat author and add "1" to avoid an ID of "0". */
+		return absint( array_search( $chat_author, $_tamatebako_chat_ids ) ) + 1;
+
+	}
+	else{
+		return '0';
+	}
 }
