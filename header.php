b81dc5cd1128f59e971dc6a3321389a2e70ab1b3
David Chandra Purnama <david@shellcreeper.com>
2015-08-02 09:23:52 +0700

    site title and desc tag.

--- header.php
+++ header.php
@@ -4,19 +4,41 @@
 
 		<?php if ( current_theme_supports( 'tamatebako-logo' ) && get_theme_mod( 'logo' ) ) { ?>
 
-			<h1 id="site-logo">
-				<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-					<img class="logo-img" src="<?php echo esc_url( tamatebako_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
-					<span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
-				</a>
-			</h1>
+			<?php if( is_front_page() && is_home() ){ ?>
 
-		<?php } else { ?>
+				<h1 id="site-logo">
+					<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img class="logo-img" src="<?php echo esc_url( tamatebako_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
+						<span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
+					</a>
+				</h1>
 
-			<h1 id="site-title"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 id="site-description"><?php bloginfo( 'description' ); ?></h2>
+			<?php } else { ?>
 
-		<?php } /* End If */ ?>
+				<p id="site-logo">
+					<a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img class="logo-img" src="<?php echo esc_url( tamatebako_logo_url() ); ?>" alt="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name' ) ); ?>"/>
+						<span class="screen-reader-text"><?php bloginfo( 'name' ); ?></span>
+					</a>
+				</p>
+
+			<?php } /* end logo */ ?>
+
+		<?php } else { /* no logo */ ?>
+
+			<?php if( is_front_page() && is_home() ){ ?>
+
+				<h1 id="site-title"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+
+			<?php } else { ?>
+
+				<p id="site-title"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+
+			<?php } ?>
+
+			<p id="site-description"><?php bloginfo( 'description' ); ?></p>
+
+		<?php } /* end logo conditional */ ?>
 
 	</div><!-- #branding -->
 
