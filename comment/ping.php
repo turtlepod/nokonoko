39deef05cafac2318bb67a144a4439749d75020a
David Chandra Purnama <david@shellcreeper.com>
2015-08-06 15:21:56 +0700

    update ping markup.

--- comment/ping.php
+++ comment/ping.php
@@ -4,9 +4,7 @@
 
 		<cite class="comment-author"><?php comment_author_link(); ?></cite><br />
 
-		<time class="comment-published" datetime="<?php echo get_comment_time( 'Y-m-d\TH:i:sP' );?>"><?php printf( '%1$s (%2$s)', get_comment_date(), get_comment_time() ) ?></time>
-
-		<a class="comment-permalink" href="<?php echo esc_url( get_comment_link() ); ?>">#</a>
+		<a class="comment-permalink" href="<?php echo esc_url( get_comment_link() ); ?>"><time class="comment-published" datetime="<?php echo get_comment_time( 'Y-m-d\TH:i:sP' );?>"><?php printf( '%1$s (%2$s)', get_comment_date(), get_comment_time() ) ?></time></a>
 
 		<?php edit_comment_link(); ?>
 
