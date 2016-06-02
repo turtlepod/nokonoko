be2416477170f24199ff0cb1566162265c5320a8
David Chandra <david@shellcreeper.com>
2016-06-02 19:19:02 +0700

    add social links menus with base design.

---
+++ menu/social.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Social Menu
+ * @links http://css-tricks.com/snippets/wordpress/remove-li-elements-from-output-of-wp_nav_menu/
+ */
+if ( tamatebako_is_menu_registered( 'social' ) ){
+	$args = array(
+		'theme_location'  => 'social',
+		'container'       => false,
+		'echo'            => false,
+		'items_wrap'      => '<ul class="social-links social-links-background">%3$s</ul>',
+		'depth'           => 1,
+		'link_before'     => '<span class="screen-reader-text">',
+		'link_after'      => '</span>',
+		'fallback_cb'     => '__return_false',
+	);
+	?>
+	<div id="social-links">
+		<?php echo wp_nav_menu( $args ); ?>
+	</div>
+	<?php
+}
