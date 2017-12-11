7a063f81894cfa5aa2f288557d964867cdae532d
David <david@shellcreeper.com>
2017-12-12 05:04:20 +0700

    remove upsell module

--- library/tamatebako.php
+++ library/tamatebako.php
@@ -12,56 +12,56 @@
  * @license   http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 **/
 
-/**e
+/**
  * Start Tamatebako!
  * Setup empty object to use when needed.
  */
 global $tamatebako;
 $tamatebako = new stdClass;
 
-/* Add theme name in the object. */
+// Add theme name in the object.
 $tamatebako->name = esc_attr( get_template() );
 $tamatebako->child = esc_attr( get_stylesheet() );
 
-/* Add tamatebako directory/folder (not path) in the object. */
+// Add tamatebako directory/folder (not path) in the object.
 $tamatebako->dir = basename( dirname( __FILE__ ) );
 
 /* === LOAD FUNCTIONS === */
 
-/* Load File Loader. */
+// Load File Loader.
 require_once( trailingslashit( get_template_directory() ) . $tamatebako->dir . '/functions/helper.php' );
 
-/* Load text string used within the framework. */
+// Load text string used within the framework.
 tamatebako_include( 'functions/strings', true );
 
-/* Load various sanitization functions. */
+// Load various sanitization functions.
 tamatebako_include( 'functions/sanitize', true );
 
-/* Load default setup and helper functions. */
+// Load default setup and helper functions
 tamatebako_include( 'functions/setup', true );
 
-/* Load template-tag functions on site front end. */
+// Load template-tag functions on site front end.
 if ( !is_admin() ) {
 
-	/* General */
+	// General.
 	tamatebako_include( 'functions/template/general', true );
 
-	/* Navigation Menu */
+	// Navigation menus.
 	tamatebako_include( 'functions/template/menu', true );
 
-	/* Sidebar */
+	// Sidebar.
 	tamatebako_include( 'functions/template/sidebar', true );
 
-	/* Entry */
+	// Entry.
 	tamatebako_include( 'functions/template/entry', true );
 
-	/* Attachment */
+	// Attachment.
 	tamatebako_include( 'functions/template/attachment', true );
 
-	/* Comment */
+	// Comment.
 	tamatebako_include( 'functions/template/comment', true );
 
-	/* Load front-end utility functions for faster development ( min PHP 5.3 ) */
+	// Load front-end utility functions for faster development ( min PHP 5.3 ).
 	if ( version_compare( PHP_VERSION, '5.3.0', '>=' ) ) {
 		tamatebako_include( 'functions/template/utility', true );
 	}
@@ -69,45 +69,43 @@ if ( !is_admin() ) {
 
 /* === LOAD MODULES === */
 
-/* Load custom theme features. */
+// Load custom theme features.
 add_action( 'after_setup_theme', 'tamatebako_load_theme_support', 15 );
 
 /**
  * Load various modules only when theme add supports for it.
  * 
  * @since 3.0.0
- * @return void
  */
-function tamatebako_load_theme_support(){
+function tamatebako_load_theme_support() {
 
-	/* === BACK COMPAT === */
+	// Back compat.
 	tamatebako_require_if_theme_supports( 'tamatebako-back-compat', 'modules/back-compat' );
 
-	/* === REGISTER SIDEBARS === */
+	// Register sidebars.
 	tamatebako_require_if_theme_supports( 'tamatebako-sidebars', 'modules/sidebars' );
 
-	/* === POST FORMATS SETUP === */
-	if( !is_admin() ) tamatebako_require_if_theme_supports( 'post-formats', 'modules/post-formats' );
+	// Post formats setup.
+	if ( ! is_admin() ) {
+		tamatebako_require_if_theme_supports( 'post-formats', 'modules/post-formats' );
+	}
 
-	/* === FULL SIZE BACKGROUND === */
+	// Full size background.
 	if ( current_theme_supports( 'custom-background' ) ){
 		tamatebako_require_if_theme_supports( 'tamatebako-full-size-background', 'modules/full-size-background' );
 	}
 
-	/* === MICRODATA FILTERS === */
-	if( !is_admin() ) tamatebako_require_if_theme_supports( 'tamatebako-microdata', 'modules/microdata' );
+	// Microdata filters.
+	if ( ! is_admin() ) {
+		tamatebako_require_if_theme_supports( 'tamatebako-microdata', 'modules/microdata' );
+	}
 
-	/* === LAYOUTS === */
+	// Layouts.
 	tamatebako_require_if_theme_supports( 'tamatebako-layouts', 'modules/layouts/layouts' );
 
-	/* === CUSTOM FONTS === */
+	// Custom fonts.
 	tamatebako_require_if_theme_supports( 'tamatebako-custom-fonts', 'modules/custom-fonts/custom-fonts' );
 
-	/* === HIDE PAGE TITLE === */
+	// Hide page title.
 	tamatebako_require_if_theme_supports( 'tamatebako-hide-page-title', 'modules/hide-page-title' );
-
-	/* === UPSELL === */
-	if( !is_multisite() ){
-		tamatebako_require_if_theme_supports( 'tamatebako-upsell', 'modules/upsell' );
-	}
 }
